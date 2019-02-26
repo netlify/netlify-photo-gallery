2248feb019110f3aa34d251f69062d859476f130
Keiko Oda <keiko713@gmail.com>
2019-02-25 23:38:46 -0800

    Show loading message

--- static/js/custom.js
+++ static/js/custom.js
@@ -5,9 +5,16 @@ function applyParams() {
     var param = "?nf_resize=" + r.value + "&w=" + w.value + "&h=" + h.value;
     var paramInput = document.getElementById("exampleParams");
     paramInput.value = param;
-    var img = document.getElementById('exampleApple');
+    var msg = document.getElementById("loadingMessage");
+    var img = document.getElementById("exampleApple");
     var srcWithoutParam = img.src.split("?")[0];
-    img.src = srcWithoutParam + param;
+    var newImg = new Image();
+    newImg.onload = function(){
+        img.src = this.src;
+        msg.innerText = "";
+    };
+    newImg.src = srcWithoutParam + param;
+    msg.innerText = "loading apple...";
     return false;
 }
 
